63ea50b34f1e9b3007ff10acd027a086d5a63a95
xp664221812 <realxp141414@163.com>
2017-07-31 18:10:48 +0800

    first commit

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice06LightingColorFilterView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice06LightingColorFilterView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -35,12 +37,27 @@ public class Practice06LightingColorFilterView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+
+        /*
+        R' = R * mul.R / 0xff + add.R
+        G' = G * mul.G / 0xff + add.G
+        B' = B * mul.B / 0xff + add.B
+        **/
+
+        ColorFilter colorFilter1 = new LightingColorFilter(0x00ffff, 0x000000);
+
+        ColorFilter colorFilter2 = new LightingColorFilter(0xffffff, 0x003000);
+
+
         // 使用 Paint.setColorFilter() 来设置 LightingColorFilter
 
         // 第一个 LightingColorFilter：去掉红色部分
+        paint.setColorFilter(colorFilter1);
         canvas.drawBitmap(bitmap, 0, 0, paint);
 
+
         // 第二个 LightingColorFilter：增强绿色部分
+        paint.setColorFilter(colorFilter2);
         canvas.drawBitmap(bitmap, bitmap.getWidth() + 100, 0, paint);
     }
 }
