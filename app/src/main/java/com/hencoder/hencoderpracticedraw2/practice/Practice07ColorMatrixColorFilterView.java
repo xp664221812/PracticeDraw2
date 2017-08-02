5d409c94c27d3bd5b79dadbfdc8b914bffd517ea
xp664221812 <realxp141414@163.com>
2017-08-03 07:36:23 +0800

    xxxx

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice07ColorMatrixColorFilterView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice07ColorMatrixColorFilterView.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -29,6 +32,14 @@ public class Practice07ColorMatrixColorFilterView extends View {
 
     {
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.batman);
+
+
+        ColorMatrix colorMatrix=new ColorMatrix();
+        colorMatrix.setSaturation(0);
+
+        ColorFilter colorFilter=new ColorMatrixColorFilter(colorMatrix);
+
+        paint.setColorFilter(colorFilter);
 
         // 使用 setColorFilter() 设置一个 ColorMatrixColorFilter
         // 用 ColorMatrixColorFilter.setSaturation() 把饱和度去掉
